6043db852631998af0ec2089a242d731f02beb08
Karim Hamga <karimhamga@macbook-pro-de-karim-2.home>
2022-11-11 18:03:33 +0100

    price filter bug to add label

--- src/components/PriceRange.js
+++ src/components/PriceRange.js
@@ -1,4 +1,4 @@
-import { Range } from "react-range";
+import { Range, getTrackBackground } from "react-range";
 
 const PriceRange = ({ priceRange, setPriceRange }) => {
   const handlePriceChange = (values) => {
@@ -7,6 +7,9 @@ const PriceRange = ({ priceRange, setPriceRange }) => {
     newPriceRange[1] = values[1];
     setPriceRange(newPriceRange);
   };
+
+  const values = priceRange;
+  console.log(priceRange);
 
   return (
     <div>
@@ -25,10 +28,26 @@ const PriceRange = ({ priceRange, setPriceRange }) => {
               ...props.style,
               height: "6px",
               width: "100%",
-              backgroundColor: "#ccc",
+              backgroundColor: "#cccccc",
             }}
           >
-            {children}
+            <div
+              ref={props.ref}
+              style={{
+                height: "5px",
+                width: "100%",
+                borderRadius: "4px",
+                background: getTrackBackground({
+                  values,
+                  colors: ["#ccc", "#548BF4", "#ccc"],
+                  min: priceRange[0],
+                  max: priceRange[1],
+                }),
+                alignSelf: "center",
+              }}
+            >
+              {children}
+            </div>
           </div>
         )}
         renderThumb={({ props }) => (
@@ -36,9 +55,10 @@ const PriceRange = ({ priceRange, setPriceRange }) => {
             {...props}
             style={{
               ...props.style,
-              height: "42px",
-              width: "42px",
-              backgroundColor: "#999",
+              height: "15px",
+              width: "15px",
+              borderRadius: "50%",
+              backgroundColor: "#007580",
             }}
           />
         )}
